fc51e994311450585ca799c3fab3786163c3ce7d
Sajib Hossain <sajib.sj359@gmail.com>
2022-02-17 15:46:17 +0600

    Responsibe done

--- js/app.js
+++ js/app.js
@@ -1,20 +1,19 @@
-let income = document.getElementById('input-num').value;
-let food =document.getElementById('exp-food').value;
-let rent = document.getElementById('exp-rent').value;
-let transport = document.getElementById('exp-transport').value;
+let income = document.getElementById('input-num');
+let food =document.getElementById('exp-food');
+let rent = document.getElementById('exp-rent');
+let transport = document.getElementById('exp-transport');
 
 console.log("all done");
 
 function calculate(){
-    if(income!==0 && income!== -1){
-        console.log('dhukche')
-        console.log(income);
-        console.log(food);
-        console.log(rent);
-        console.log(transport);
-
-        let balance = parseFloat(income) - parseFloat(food) - parseFloat(rent) - parseFloat(transport); //income-(food+rent+transport);
+    if(income!= 0 && income!= -1){
+console.log('gygjg')
+        let balance = parseFloat(income.value) - parseFloat(food.value) - parseFloat(rent.value) - parseFloat(transport.value); 
         console.log(balance);
+
+        let totalExp = parseFloat(food.value) + parseFloat(rent.value) + parseFloat(transport.value);
+        document.getElementById('bal').innerText = balance;
+        document.getElementById('exp').innerText=totalExp;
     }
     else{
         console.log("your balance can't negative or zero... plz enter your balance");
@@ -24,4 +23,20 @@ function calculate(){
 document.getElementById('cal-btn').addEventListener('click',function () {
 
     calculate();
+
+})
+
+
+let saveInput = document.getElementById('save-input');
+let saveCal = document.getElementById('save-btn');
+
+function saveCalculation (){
+    if(balance!=0 && balance!=-1){
+        let savings = parseInt(balance) /  parseInt(saveInput.value)*100;
+        document.getElementById('interest').innerText = savings;
+    }
+}
+
+document.getElementById('save-btn').addEventListener('click',function(){
+
 })
