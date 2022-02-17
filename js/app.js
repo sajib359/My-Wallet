15f81644d3cc48e263e6ce9a25b6078008e842dd
Sajib Hossain <sajib.sj359@gmail.com>
2022-02-17 18:45:17 +0600

    add validation

--- js/app.js
+++ js/app.js
@@ -1,42 +1,62 @@
-let income = document.getElementById('input-num');
-let food =document.getElementById('exp-food');
-let rent = document.getElementById('exp-rent');
-let transport = document.getElementById('exp-transport');
+let incomeField = document.getElementById('input-num');
+let foodField = document.getElementById('exp-food');
+let rentField = document.getElementById('exp-rent');
+let transportField = document.getElementById('exp-transport');
+let saveField = document.getElementById('save-btn');
 
-console.log("all done");
-
-function calculate(){
-    if(income!= 0 && income!= -1){
-console.log('gygjg')
-        let balance = parseFloat(income.value) - parseFloat(food.value) - parseFloat(rent.value) - parseFloat(transport.value); 
-        console.log(balance);
-
-        let totalExp = parseFloat(food.value) + parseFloat(rent.value) + parseFloat(transport.value);
-        document.getElementById('bal').innerText = balance;
-        document.getElementById('exp').innerText=totalExp;
+function checkInput() {
+    if (isNaN(parseFloat(incomeField.value))) {
+        document.getElementById("income-alt").style.display = "block";
+    } else {
+        document.getElementById("income-alt").style.display = "none";
     }
-    else{
-        console.log("your balance can't negative or zero... plz enter your balance");
+    // checking food field 
+    if (isNaN(parseFloat(foodField.value))) {
+        document.getElementById("food-alt").style.display = "block";
+    } else {
+        document.getElementById("food-alt").style.display = "none";
+
+    }
+    // checking rent field 
+    if (isNaN(parseFloat(rentField.value))) {
+        document.getElementById("rent-alt").style.display = "block";
+    } else {
+        document.getElementById("rent-alt").style.display = "none";
+    }
+    // checking cloth field 
+    if (isNaN(parseFloat(transportField.value))) {
+        document.getElementById("transport-alt").style.display = "block";
+    } else {
+        document.getElementById("transport-alt").style.display = "none";
+    }
+
+    // end calculation if any number error 
+    if(isNaN(parseFloat(incomeField.value)) || isNaN(parseFloat(foodField.value)) || isNaN(parseFloat(rentField.value)) || isNaN(parseFloat(transportField.value))){
+        return false;
+    } else{
+        return true;
+    }
+
+}
+
+function checkSave() {
+    // checking saving ammount field 
+    if (isNaN(parseFloat(saveField.value))) {
+        document.getElementById("saveWarn").style.display = "block";
+        return false;
+    } else {
+        document.getElementById("saveWarn").style.display = "none";
+        return true;
     }
 }
 
-document.getElementById('cal-btn').addEventListener('click',function () {
-
-    calculate();
-
-})
-
-
-let saveInput = document.getElementById('save-input');
-let saveCal = document.getElementById('save-btn');
-
-function saveCalculation (){
-    if(balance!=0 && balance!=-1){
-        let savings = parseInt(balance) /  parseInt(saveInput.value)*100;
-        document.getElementById('interest').innerText = savings;
+document.getElementById('cal-btn').addEventListener('click',function(){
+    checkInput();
+    if(checkInput() == true){
+    let balance = parseFloat(incomeField.value) - parseFloat(foodField.value) - parseFloat(rentField.value) - parseFloat(transportField.value);
+     console.log(balance);
+     let totalExp = parseFloat(foodField.value) + parseFloat(rentField.value) + parseFloat(transportField.value);
+     document.getElementById('bal').innerText = balance;
+     document.getElementById('exp').innerText=totalExp; 
     }
-}
-
-document.getElementById('save-btn').addEventListener('click',function(){
-
 })
